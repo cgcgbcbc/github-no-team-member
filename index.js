df77b2925e9d79cea9b8a52207095d790246ec7c
cgcgbcbc <cgcgbcbc@163.com>
2015-11-06 01:15:37 +0800

    add verbose option

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 #!/usr/bin/env node
 function printUsage() {
-  console.log('usage: ./index.js [org]');
+  console.log('usage: ./index.js [option] [org]');
+  console.log('option:');
+  console.log('\t-v: verbose:');
 }
 
-if (process.argv.length !== 3) {
+if (process.argv.length !== 3 && process.argv.length !== 4) {
   printUsage();
   process.exit(-1);
 }
 
 var org = process.argv[2];
+var verbose = false;
+
+if (process.argv.length === 4) {
+  if (org === '-v') {
+    verbose = true;
+  } else {
+    printUsage();
+    process.exit(-1);
+  }
+  org = process.argv[3];
+}
 
 var GitHubApi = require('github');
 var Config = require('nodegit').Config;
@@ -39,14 +52,28 @@ Rx.Observable.fromPromise(Config.openDefault()).flatMap(function(config) {
     Rx.Observable.fromNodeCallback(github.orgs.getMembers)({org: org, per_page: 100}),
     Rx.Observable.fromNodeCallback(github.orgs.getTeams)({org: org}),
     function(members, teams) {
+      if (verbose) {
+        console.log('members:');
+        console.log(members);
+        console.log('teams:');
+        console.log(teams);
+      }
       return {members: members, teams: teams};
     }
   );
 }).flatMap(function(org) {
   return Rx.Observable.zip(
     Rx.Observable.fromArray(org.teams).flatMap(function(team) {
+      if (verbose) {
+        console.log('team:');
+        console.log(team);
+      }
       return Rx.Observable.fromNodeCallback(github.orgs.getTeamMembers)({id: team.id});
     }).flatMap(function(teamMembers) {
+      if (verbose) {
+        console.log('teamMembers:');
+        console.log(teamMembers);
+      }
       return Rx.Observable.fromArray(teamMembers);
     }).map(function(teamMember) {
       return teamMember.login;
@@ -57,6 +84,10 @@ Rx.Observable.fromPromise(Config.openDefault()).flatMap(function(config) {
     }
   );
 }).flatMap(function(data) {
+  if (verbose) {
+    console.log('data');
+    console.log(data);
+  }
   var filter = data.filter;
   return Rx.Observable.fromArray(data.all).filter(function (x) {
     return filter.indexOf(x.login) === -1;
